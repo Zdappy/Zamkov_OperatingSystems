8703b8db0506d731ed7af81b72814c3fe0bf59e0
Zdappy <mishanskiy.96@gmail.com>
2026-04-27 20:25:35 +0800

    Create factorial.h

---
+++ include/factorial.h
@@ -0,0 +1,10 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+// Функция для вычисления факториала (рекурсивная)
+unsigned long long factorial(int n);
+
+// Функция для вычисления факториала (итеративная)
+unsigned long long factorial_iterative(int n);
+
+#endif // FACTORIAL_H
