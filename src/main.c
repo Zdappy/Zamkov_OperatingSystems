7eaf1187ce4519eafdb501ea01bedf179eba889d
Zdappy <mishanskiy.96@gmail.com>
2026-04-27 20:20:24 +0800

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "factorial.h"
+
+int main() {
+    int numbers[] = {5, 7, 10};
+    int count = sizeof(numbers) / sizeof(numbers[0]);
+    printf("Вычисление факториала:\n");
+    
+    for (int i = 0; i < count; i++) {
+        int n = numbers[i];
+        unsigned long long result = factorial(n);
+        printf("Factorial(%d) = %llu\n", n, result);
+    }
+    
+    return 0;
+}
